b28a007c0d8c032f457e7142971d6ffde7f934a1
DAVIS ISIBOR <davisisibor@gmail.com>
2024-07-12 01:39:17 +0100

    update

--- src/app/components/general-component/nav.tsx
+++ src/app/components/general-component/nav.tsx
@@ -2,6 +2,7 @@
 
 import { Bt_Beau_Regualr } from "@/app/utils/fonts";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Nav = () => {
   const items = [
@@ -36,6 +37,8 @@ const Nav = () => {
       link: "/",
     },
   ];
+
+  const pathname = usePathname();
   return (
     <>
       <nav
@@ -49,10 +52,20 @@ const Nav = () => {
                 href={e.link}
                 style={{ whiteSpace: "nowrap" }}
                 key={index}
-                className="uppercase overflow-hidden  md:p-[0.5vw] group hover:[#103210]  hover:bg-[black] hover:bg-opacity-[20%]  md:rounded-[1.5vw] bg-[white] backdrop-blur-2xl bg-opacity-[20%] "
+                className={` ${
+                  e.link == pathname ? "bg-[black]" : "bg-[white]"
+                } uppercase overflow-hidden  md:p-[0.5vw] group hover:[#103210]  hover:bg-[black] hover:bg-opacity-[20%]  md:rounded-[1.5vw]  backdrop-blur-2xl bg-opacity-[20%] `}
               >
-                <div className="w-full h-full bg-white group-hover:bg-[#103210] md:rounded-[1.3vw] flex justify-center items-center  md:py-[0.6vw] md:px-[1.5vw]">
-                  <p className="inline-block md:text-[0.8vw] text-[#103210] group-hover:text-white">
+                <div
+                  className={`w-full  ${
+                    e.link == pathname ? "bg-[#103210]" : "bg-white"
+                  } h-full  group-hover:bg-[#103210] md:rounded-[1.3vw] flex justify-center items-center  md:py-[0.6vw] md:px-[1.5vw]`}
+                >
+                  <p
+                    className={`inline-block  ${
+                      e.link == pathname ? "text-white" : "text-[#103210]"
+                    }  md:text-[0.8vw] text-[#103210] group-hover:text-white`}
+                  >
                     {e.text}
                   </p>
                 </div>
