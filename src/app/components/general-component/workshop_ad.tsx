ba6b526096c7b9b589d6502d45a0dd17aa266f4b
DAVIS ISIBOR <davisisibor@gmail.com>
2024-07-14 18:42:20 +0100

    update contents

--- src/app/components/general-component/workshop_ad.tsx
+++ src/app/components/general-component/workshop_ad.tsx
@@ -3,11 +3,16 @@
 import { useMotionValueEvent, useScroll, useTransform } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
 
+import example from "../../../../public/images/workshop/example1.webp";
+import example2 from "../../../../public/images/workshop/example2.webp";
+import example3 from "../../../../public/images/workshop/example3.webp";
+import Image from "next/image";
+import { Helvetica_bold, Helvetica_light } from "@/app/utils/fonts";
+import Link from "next/link";
 const Workshop_ad = () => {
   const sectionRef = useRef(null);
   const { scrollYProgress } = useScroll({
     target: sectionRef,
-    // offset: ["end start", "end 50%"],
   });
   const width = globalThis.innerWidth;
   useEffect(() => {
@@ -17,14 +22,30 @@ const Workshop_ad = () => {
   const [yvalue, setyvalue] = useState(1);
   const [scale_y2value, setscale_y2value] = useState(0);
   const [height, setheight] = useState(1);
-
-  const y = useTransform(scrollYProgress, [0, 1], [1, 4.5]);
+  const data_array = [
+    {
+      bg: "#CBD4CB",
+      img: example,
+      heading:
+        "Negotiation Fundamentals: Building the Foundation for Strategic Mastery",
+    },
+    {
+      bg: "#D4CBCB",
+      heading: "Growing the Pie: Elegant Trades and Creativity",
+      img: example2,
+    },
+    {
+      bg: "#D4D4CB",
+      heading: "Cutting the Pie: Confident Deal Making",
+      img: example3,
+    },
+  ];
+  const y = useTransform(scrollYProgress, [0, 1], [1, data_array.length + 0.5]);
   const scale_y = useTransform(scrollYProgress, [0, 1], [10, 1]);
   const parent_height = useTransform(scrollYProgress, [0, 1], [1, 10]);
 
   useMotionValueEvent(y, "change", (latest) => {
     setyvalue(latest);
-    // console.log(latest);
   });
   useMotionValueEvent(scale_y, "change", (latest) => {
     setscale_y2value(latest);
@@ -33,36 +54,36 @@ const Workshop_ad = () => {
     setheight(latest);
   });
 
-  const data_array = ["", "", "", ""];
+  //   const calculateScale = (index: number, data_array: any[], yvalue: number) => {
+  //     const minScale = Math.min(...data_array) / 2 + scale_y2value / 10;
+  //     const maxScale = 1;
+  //     const scaleFactor = (yvalue - (index + 1)) / 6; // adjust this value to control the scaling speed
+  //     const scale = maxScale - (maxScale - minScale) * scaleFactor;
 
-  // const calculateScale = (index: number, data_array: any[], yvalue: number) => {
-  //   const minScale = Math.min(...data_array) / 2 + scale_y2value / 10;
-  //   const scale = (index / (data_array.length - 1)) * (1 - minScale) + minScale;
+  //     return scale;
+  //   };
 
-  //   return scale;
-  // };
-
-  const calculateScale = (index: number, data_array: any[], yvalue: number) => {
-    const minScale = Math.min(...data_array) / 2 + scale_y2value / 10;
+  const calculateScale = (index: any, data_array: any, yvalue: any) => {
+    const minScale = 0.6;
     const maxScale = 1;
-    const scaleFactor = (yvalue - (index + 1)) / 6; // adjust this value to control the scaling speed
+    const scaleFactor = (yvalue - (index + 1)) / 6;
     const scale = maxScale - (maxScale - minScale) * scaleFactor;
-
-    return scale;
+    return Math.max(minScale, Math.min(maxScale, scale)); // Ensure scale stays within bounds
   };
   return (
     <>
       {/* the wrapper */}
       <div
-        className="w-full lg:h-[150vw] flex items-end h-[300vh] md:h-[300vh] bg-[#F3FEFF] relative"
+        className="w-full  flex items-end h-[300vh] md:h-[300vh] bg-[#DFE4DF] relative"
         ref={sectionRef}
+        style={{ height: `${data_array.length * 110}vh ` }}
       >
-        <div className="flex justify-center items-center   sticky bottom-0 h-[100vh] bg-[#F3FEFF] w-full ">
+        <div className="flex justify-center items-center   sticky bottom-0 h-[100vh] bg-[#DFE4DF] w-full ">
           {/* the customize scroll bar starts */}
-          <div className="absolute md:right-[3vw] z-[10] border-[#0e257756]  flex w-[2%] right-[1.5%]  top-[50%] translate-y-[-50%] md:w-[0.6vw] rounded-[3vw]  lg:h-[28vw] md:h-[40vw] h-[140vw] bg-white overflow-hidden">
+          <div className="absolute md:right-[3vw] z-[10] border-[#0e257756]  flex w-[2%] right-[1.5%]  top-[50%] translate-y-[-50%] md:w-[0.6vw] rounded-[3vw]  lg:h-[25vw] md:h-[30vw] h-[140vw] bg-white   overflow-hidden">
             <div
               className="w-full rounded-[3vw] bg-[#0E2477]"
-              style={{ height: `${height * 10}%`, transition: "0.2s" }}
+              style={{ height: `${height * 10}%` }}
             ></div>
           </div>
           {/* the customized scroll bar ends */}
@@ -70,12 +91,9 @@ const Workshop_ad = () => {
             return (
               <div
                 key={index}
-                className={`lg:w-[80vw] absolute top-[50%] translate-x-[-50%] left-[50%]  lg:rounded-[2vw] md:w-[85vw] w-[90%] h-[150vw] md:h-[40vw] md:rounded-[3vw] lg:h-[30vw] overflow-hidden rounded-[7.5vw]  bg-[#0E2477] flex  ${
-                  index % 2 === 0
-                    ? "md:flex-row flex-col"
-                    : "md:flex-row-reverse flex-col"
-                } justify-between items-center md:p-[2vw] p-[4vw] gap-[4vw] md:gap-[2vw] text-white`}
+                className={` absolute top-[50%] translate-x-[-50%] left-[50%]   md:w-[90vw] w-[90%] h-[150vw] md:h-[30vw] lg:h-[25vw] md:rounded-[2vw]  overflow-hidden rounded-[7.5vw]   flex   justify-between items-center  text-white`}
                 style={{
+                  backgroundColor: e.bg,
                   transition: "opacity 0.6s ease, filter  jjj0.6s ease",
                   opacity:
                     index + 1 - yvalue >= 0 && index + 1 - yvalue <= 1
@@ -92,7 +110,7 @@ const Workshop_ad = () => {
                       ? `translateY(${
                           -50 -
                           (yvalue - (index + 1)) *
-                            (calwidth < 765 ? 15 / 1.5 : 15 / 1.2)
+                            (calwidth < 765 ? 15 / 1.5 : 9 / 1.2)
                           // i am changing this value for scale (15/#change)
                         }%) translateX(-50%)  scale(${calculateScale(
                           index,
@@ -105,19 +123,50 @@ const Workshop_ad = () => {
                         }%) translateX(-50%)`,
 
                   filter:
-                    index + 1 - yvalue >= 0 && index + 1 - yvalue <= 1
-                      ? ``
-                      : index + 1 - yvalue <= -0.8
-                      ? `blur(${(yvalue - index) * 1.2}px)`
-                      : ``,
+                    index != data_array.length - 1
+                      ? index + 1 - yvalue >= 0 && index + 1 - yvalue <= 1
+                        ? ``
+                        : index + 1 - yvalue <= -0.8
+                        ? `blur(${(yvalue - index) * 1.2}px)`
+                        : ``
+                      : "",
                 }}
-              ></div>
+              >
+                <div className="w-[50%] h-full   relative">
+                  <Image
+                    src={e.img}
+                    alt={e.heading}
+                    className="absolute absolute_center   w-full h-full"
+                  />
+                </div>
+                <div className="w-[50%] h-full  md:px-[5%] justify-center  flex flex-col md:gap-[1.5vw]">
+                  <h4
+                    className={`md:text-[2.5vw] ${Helvetica_bold.className} text-[#000000] md:leading-[2.8vw]`}
+                  >
+                    {e.heading}
+                  </h4>
+
+                  <Link
+                    style={{
+                      whiteSpace: "nowrap",
+                      transition: "0.9s ease",
+                      // transform: start_anime ? "translate(0,0)" : "translate(0%,80%)",
+                    }}
+                    href={"/"}
+                    className={` ${Helvetica_light.className} uppercase overflow-hidden  md:p-[0.5vw] w-fit group hover:[#103210]  hover:bg-[black] hover:bg-opacity-[20%]  md:rounded-[2vw] bg-[black] backdrop-blur-2xl bg-opacity-[20%] `}
+                  >
+                    <div className="w-full h-full bg-[#440C0C] group-hover:bg-[#103210] md:rounded-[1.7vw] flex justify-center items-center  md:py-[0.7vw] md:px-[1.5vw]">
+                      <p className="inline-block md:text-[1vw] text-[white] group-hover:text-white">
+                        see all
+                      </p>
+                    </div>
+                  </Link>
+                </div>
+              </div>
             );
           })}
         </div>
       </div>
-
-      <div className="h-[100vh] bg-[black]"></div>
     </>
   );
 };
