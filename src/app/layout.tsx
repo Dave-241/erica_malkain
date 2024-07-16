0982c680441aaedb2d392d46fbb4b5ec618f19d0
DAVIS ISIBOR <davisisibor@gmail.com>
2024-07-16 16:07:43 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -14,9 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={``} style={{ backgroundColor: "#DFE4DF" }}>
-        {children}
-      </body>
+      <body style={{ backgroundColor: "#DFE4DF" }}>{children}</body>
     </html>
   );
 }
