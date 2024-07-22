e85b157232cb52fc6f45a5ad8b004d3e9b2bbe44
DAVIS ISIBOR <davisisibor@gmail.com>
2024-07-22 14:19:22 +0100

    Update recent_publication.tsx

--- src/app/components/general-component/recent_publication.tsx
+++ src/app/components/general-component/recent_publication.tsx
@@ -106,13 +106,15 @@ const Recent_publication = () => {
                 }}
                 onMouseEnter={() => setactive(index)}
                 onMouseLeave={() => setactive(null)}
-                className={`w-full overflow-hidden md:h-[37vw]  relative ${
-                  active == index ? "md:border-none" : ""
-                } flex-col  flex justify-end      group border-b-[#565956] border-b-[0.1vw] `}
+                className={`w-full overflow-hidden  md:h-[37vw] h-[95vw]  relative ${
+                  active == index
+                    ? "md:border-none rounded-[2vw] md:rounded-none md:px-0 px-[5%]"
+                    : ""
+                } flex-col  flex justify-end   group border-b-[#565956] border-b-[0.1vw] `}
               >
                 <div
-                  className={` md:w-[46.5%]  flex justify-center  items-center  ${
-                    active == index ? "md:bottom-0" : "md:bottom-[100%]"
+                  className={` md:w-[46.5%]  h-[95vw] w-full left-0 flex justify-center  items-center  ${
+                    active == index ? "bottom-0" : "bottom-[100%]"
                   } absolute md:h-[37vw] overflow-hidden md:rounded-[1vw]`}
                   style={{
                     transition: "0.7s ease",
@@ -123,18 +125,21 @@ const Recent_publication = () => {
                     <Image
                       src={example}
                       alt={e.title}
-                      style={
-                        {
-                          // transform: "translateX(-50%) translateY(-50%) ",
-                        }
-                      }
                       className={` w-full absolute h-fit left-[50%] top-[50%] translate-x-[-50%] scale-[1.1] translate-y-[-50%] `}
                     />
                   </div>
                 </div>
 
+                <div
+                  style={{
+                    transition: "0.7s ease",
+                  }}
+                  className={`w-full ${
+                    active == index ? "h-[70%]" : "h-[0%]"
+                  } md:h-[0%] bg-gradient-to-t  from-[#000000] absolute bottom-0 left-0 `}
+                ></div>
+                {/* the mobile overlay  */}
                 {/* first section which includes the s/n and also the title */}
-
                 <div
                   className="w-full  research_initial py-[6vw] md:flex-row flex-col  gap-[5vw]  md:gap-[10vw] md:py-[1.2vw] border-opacity-[50%] flex justify-between   "
                   ref={(ref) => {
@@ -151,16 +156,36 @@ const Recent_publication = () => {
                       active == index
                         ? "md:translate-x-[160%] md:translate-y-[-130%]"
                         : ""
-                    } md:w-[35%] md:text-[1.2vw] text-[4.5vw] md:leading-[1.8vw] leading-[5vw] items-center  ${
+                    } md:w-[35%] md:text-[1.2vw] text-[4.5vw] z-[20] md:leading-[1.8vw] leading-[5vw] items-center  ${
                       Helvetica_medium.className
                     }`}
                   >
-                    <h2 className={`text-[#000000]  uppercase`}>{e.title}</h2>
+                    <h2
+                      style={{
+                        transition: "0.7s ease",
+                      }}
+                      className={`  ${
+                        active == index
+                          ? "text-[white] md:text-opacity-[100%] text-opacity-[70%] md:text-[#000000]"
+                          : "text-[#000000] "
+                      } z-[20] uppercase`}
+                    >
+                      {e.title}
+                    </h2>
                   </div>
                   {/* this includes body  and arrow  */}
-                  <div className="flex md:gap-[1vw] md:w-[60%] justify-between md:justify-end items-center ">
+                  <div className="flex md:gap-[1vw] gap-[2vw] z-[20] md:w-[60%] justify-between md:justify-end items-center ">
                     <p
-                      className={`text-[black] md:w-[80%] w-[90%] text-[3.5vw] md:leading-[1.5vw] leading-[4.15vw] md:text-[1.1vw]  ${Helvetica_light.className}`}
+                      style={{
+                        transition: "0.7s ease",
+                      }}
+                      className={` ${
+                        active == index
+                          ? "text-[white] md:text-opacity-[100%] text-opacity-[70%] md:text-[black]"
+                          : "text-[black]"
+                      } md:w-[80%] w-[90%] z-[20] text-[3.5vw] md:leading-[1.5vw] leading-[4.15vw] md:text-[1.1vw]  ${
+                        Helvetica_light.className
+                      }`}
                     >
                       {e.body}
                       {/* 434543 */}
@@ -169,20 +194,28 @@ const Recent_publication = () => {
                     <Link
                       href={"/"}
                       style={{ whiteSpace: "nowrap" }}
-                      className={`flex justify-center bg-transparent  w-[10vw] h-[10vw] items-center overflow-hidden border-opacity-[50%]  border-[black] border-[0.1vw] rounded-[100%]  md:w-[2.5vw] md:h-[2.5vw]  relative`}
+                      className={`flex justify-center bg-transparent  w-[10vw] h-[10vw] items-center  ${
+                        active == index
+                          ? "md:border-[black] border-[#ffffff7b]"
+                          : "border-[black]"
+                      }  overflow-hidden border-opacity-[50%]   border-[0.1vw] rounded-[100%]  md:w-[2.5vw] md:h-[2.5vw]  relative`}
                     >
                       <Image
                         src={img_black}
                         alt="arrow image"
                         style={{ transition: "0.5s ease" }}
-                        className="w-[60%]  opacity-[100%] absolute absolute_center z-[10] h-fit"
+                        className={` w-[60%]   ${
+                          active == index ? "opacity-[0%]" : "opacity-[100%]"
+                        } absolute absolute_center md:opacity-[100%] z-[10] h-fit`}
                       />
-                      {/* <Image
+                      <Image
                         src={img_white}
                         alt="arrow image"
                         style={{ transition: "0.5s ease" }}
-                        className="w-[60%] absolute hover:opacity-[100%]  opacity-[0%] absolute_center z-[10] h-fit"
-                      /> */}
+                        className={`w-[60%] absolute md:hidden block  ${
+                          active == index ? "opacity-[100%]" : "opacity-[0%]"
+                        } hover:opacity-[100%]  opacity-[0%] absolute_center z-[10] h-fit`}
+                      />
 
                       {/* <div
                         className="w-full h-full bg-[#440C0C] absolute left-0 top-[100%] group-hover:top-0 "
