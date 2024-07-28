c6414c437057eb64d5895790355f823afa89fce5
DAVIS ISIBOR <davisisibor@gmail.com>
2024-07-28 14:40:18 +0100

    Update modal_edit_add_consultation.tsx

--- src/app/components/teaching_consultation/modal_edit_add_consultation.tsx
+++ src/app/components/teaching_consultation/modal_edit_add_consultation.tsx
@@ -65,45 +65,45 @@ const Modal_edit_consulation = ({
   return (
     <>
       {open_img && <Image_list setopen_img={setopen_img} />}
-      <div className="w-full  h-full flex  justify-center items-center z-[1000] fixed top-0 left-0 bg-black bg-opacity-[60%] md:px-[3%]">
+      <div className="w-full   h-full flex py-[5vw]  justify-center items-start md:py-[5vw] overflow-x-hidden z-[1000] fixed top-0 left-0 bg-black bg-opacity-[60%] overflow-y-scroll md:px-[3%]">
         <div
-          className="md:w-auto w-full gap-[5vw] py-[5vw]  bg-white md:p-[2vw] justify-between flex flex-col md:h-auto md:gap-[1vw] md:rounded-[2vw]
+          className="md:w-auto w-full gap-[5vw] py-[7vw]   bg-white md:p-[2vw] justify-between flex flex-col md:h-auto md:gap-[1vw] md:rounded-[2vw]
         "
         >
-          <p className="md:text-[2vw] capitalize text-[5vw] text-center">
+          <p className="md:text-[2vw] capitalize text-[7vw] text-center">
             {" "}
-            {consultation_title ? "edit" : "Add new"} publication here
+            {consultation_title ? "edit" : "Add new"} consultation here
           </p>{" "}
-          <div className=" flex   justify-center md:flex-row flex-col   md:w-fit md:gap-[3vw]">
+          <div className=" flex gap-[10vw]   justify-center md:flex-row flex-col   md:w-fit md:gap-[3vw]">
             {/* the color section */}
             <div className="flex w-full    md:w-fit  md:text-[1vw] text-[3.5vw]   capitalize md:gap-[2vw]">
-              <div className="flex flex-col  md:gap-[1vw] text-center">
+              <div className="flex flex-col gap-[3vw] md:gap-[1vw] text-center">
                 <p className="">select backgroundColor here</p>
                 <ColorPicker
                   hideInput={["rgb", "hsv"]}
-                  height={calWidth < 768 ? 50 : 200}
+                  height={calWidth < 768 ? 100 : 200}
                   color={consultation_bg_color}
                   onChange={setconsultation_bg_color}
                 />
               </div>
-              <div className="flex flex-col  md:gap-[1vw] text-center">
+              <div className="flex flex-col gap-[3vw] md:gap-[1vw] text-center">
                 <p className="">select title color here </p>
                 <ColorPicker
                   hideInput={["rgb", "hsv"]}
                   color={consultation_heading_text_color}
-                  height={calWidth < 768 ? 50 : 200}
+                  height={calWidth < 768 ? 100 : 200}
                   onChange={setconsultation_heading_text_color}
                 />
               </div>
             </div>
 
             {/* the text section */}
-            <div className=" flex md:w-[50%] w-full md:px-0 px-[5%]  flex-col md:gap-[1.5vw]">
-              <div className=" md:flex-row gap-[4vw] flex-col flex md:items-end justify-center md:gap-[1vw]">
+            <div className=" flex md:w-[50%] gap-[5vw] w-full md:px-0 px-[3%]  flex-col md:gap-[1.5vw]">
+              <div className="   gap-[4vw] flex-col flex md:items-end justify-center md:gap-[1vw]">
                 <div className="flex w-full flex-col md:gap-[0.3vw] gap-[2vw]">
                   <label
                     htmlFor="title"
-                    className="capitalize md:text-[1vw] text-[3vw]"
+                    className="capitalize md:text-[1vw] text-[3.5vw]"
                   >
                     consulation title
                   </label>
@@ -114,36 +114,60 @@ const Modal_edit_consulation = ({
                     onChange={(e) => {
                       setconsultation_title(e.target.value);
                     }}
-                    className="border rounded-[1vw] outline-none bg-[black] bg-opacity-[70%] placeholder:text-white placeholder:text-opacity-[50%] capitalize text-white md:h-[3vw] h-[8vw] px-[3%]  md:text-[1vw] text-[3vw]"
+                    className="border rounded-[1vw] outline-none bg-[black] bg-opacity-[70%] placeholder:text-white placeholder:text-opacity-[50%] capitalize text-white md:h-[3vw] h-[8vw] px-[3%]  md:text-[1vw] text-[3.5vw]"
                     placeholder="Eg : NEGOTIATION COURSE , WORKSHOPS . . ."
                   />
                 </div>
 
-                <div className="flex w-full md:h-[3vw]  h-[10vw] md:gap-[1vw] gap-[5%] ">
-                  <button
-                    style={{ whiteSpace: "nowrap" }}
-                    className="  h-full w-full md:text-[0.9vw] md:px-[1.3vw] bg-[#103210] text-white md:rounded-[0.5vw] hover:bg-white hover:text-black text-[3vw]  rounded-[2vw] hover:border-black border-[#103210] border"
-                    onClick={() => {
-                      setopen_img(true);
-                    }}
-                  >
-                    {consultation_image_link ? "Replace" : "Choose"} Image
-                  </button>
-                  {consultation_image_link && (
-                    <div className="h-full w-full md:rounded-[0.5vw] rounded-[2vw] relative overflow-hidden">
-                      <Image
-                        src={consultation_image_link}
-                        alt="image link"
-                        className="w-full  absolute  absolute_center h-fit"
-                      />
-                    </div>
-                  )}
+                {/* these are for the buttons */}
+                <div className="w-full flex md:flex-row flex-col gap-[3vw]  ">
+                  <div className="flex w-full md:h-[3vw]  h-[10vw]  ">
+                    <button
+                      style={{ whiteSpace: "nowrap" }}
+                      className="  h-full w-full md:text-[0.7vw] md:px-[1.3vw] bg-[#103210] text-white md:rounded-[0.5vw] hover:bg-white hover:text-black text-[3.5vw]  rounded-[2vw] hover:border-black border-[#103210] border"
+                      onClick={() => {
+                        setopen_img(true);
+                      }}
+                    >
+                      {consultation_image_link ? "Replace" : "Choose"} institue
+                      image
+                    </button>
+                    {consultation_image_link && (
+                      <div className="h-full w-full md:rounded-[0.5vw] rounded-[2vw] relative overflow-hidden">
+                        <Image
+                          src={consultation_image_link}
+                          alt="image link"
+                          className="w-full  absolute  absolute_center h-fit"
+                        />
+                      </div>
+                    )}
+                  </div>
+                  <div className="flex w-full md:h-[3vw]  h-[10vw]  ">
+                    <button
+                      style={{ whiteSpace: "nowrap" }}
+                      className="  h-full w-full md:text-[0.7vw] md:px-[1.3vw] bg-[#103210] text-white md:rounded-[0.5vw] hover:bg-white hover:text-black text-[3.5vw]  rounded-[2vw] hover:border-black border-[#103210] border"
+                      onClick={() => {
+                        setopen_img(true);
+                      }}
+                    >
+                      {consultation_image_link ? "Replace" : "Choose"} bg Image
+                    </button>
+                    {consultation_image_link && (
+                      <div className="h-full w-full md:rounded-[0.5vw] rounded-[2vw] relative overflow-hidden">
+                        <Image
+                          src={consultation_image_link}
+                          alt="image link"
+                          className="w-full  absolute  absolute_center h-fit"
+                        />
+                      </div>
+                    )}
+                  </div>
                 </div>
               </div>
               <div className="flex flex-col md:mt-0 mt-[5vw] md:gap-[0.3vw] gap-[2vw]">
                 <label
                   htmlFor="description"
-                  className="capitalize md:text-[1vw] text-[3vw]"
+                  className="capitalize md:text-[1vw] text-[3.5vw]"
                 >
                   Consultation Description
                   <br />
@@ -164,18 +188,18 @@ const Modal_edit_consulation = ({
                   onChange={(e) => {
                     setconsultation_body(e.target.value);
                   }}
-                  className="border resize-none rounded-[1vw] outline-none bg-[black] bg-opacity-[70%] placeholder:text-white placeholder:text-opacity-[50%]  p-[3%] capitalize text-white  md:p-[2%] md:text-[1vw] text-[3vw]"
+                  className="border resize-none rounded-[1vw] outline-none bg-[black] bg-opacity-[70%] placeholder:text-white placeholder:text-opacity-[50%]  p-[3%] capitalize text-white  md:p-[2%] md:text-[1vw] text-[3.5vw]"
                   placeholder="Eg : Erica is a consultant with <a href='Link_To_Behavioralize'> Behavioralize </a> , a data-driven company that applies behavioral science to understand and influence customer and managerial decision making, helping companies drive growth by identifying and solving their key behavioral challenges .."
                 />
               </div>
 
               {/*  the read more link, the institute and the year */}
-              <div className="w-full flex md:gap-[2vw] gap-[3%]  justify-between">
+              <div className="  w-full flex md:flex-nowrap flex-wrap md:gap-[2vw] gap-[2%]  justify-between">
                 {/* the institure link */}
-                <div className="flex flex-col md:gap-[0.3vw] gap-[2vw] w-full">
+                <div className="flex flex-col md:gap-[0.3vw] gap-[2vw] md:w-full w-[48%]">
                   <label
                     htmlFor="institute"
-                    className="capitalize md:text-[1vw] text-[3vw]"
+                    className="capitalize md:text-[1vw] text-[3.5vw]"
                   >
                     Institute
                   </label>
@@ -186,15 +210,15 @@ const Modal_edit_consulation = ({
                     onChange={(e) => {
                       setconsultation_institute(e.target.value);
                     }}
-                    className="border rounded-[0.5vw]  w-full outline-none bg-[black] bg-opacity-[70%] placeholder:text-white placeholder:text-opacity-[50%] capitalize text-white md:h-[3vw] h-[8vw] px-[3%] md:text-[1vw] text-[3vw]"
+                    className="border rounded-[0.5vw]  w-full outline-none bg-[black] bg-opacity-[70%] placeholder:text-white placeholder:text-opacity-[50%] capitalize text-white md:h-[3vw] h-[8vw] px-[3%] md:text-[1vw] text-[3.5vw]"
                     placeholder="Eg : Wharton University, Wet Cement  .."
                   />
                 </div>
                 {/* the year  */}
-                <div className="flex flex-col md:gap-[0.3vw] gap-[2vw] w-full">
+                <div className="flex flex-col md:gap-[0.3vw] gap-[2vw] md:w-full w-[48%]">
                   <label
                     htmlFor="year"
-                    className="capitalize md:text-[1vw] text-[3vw]"
+                    className="capitalize md:text-[1vw] text-[3.5vw]"
                   >
                     year
                   </label>
@@ -205,15 +229,15 @@ const Modal_edit_consulation = ({
                       setconsultation_year(e.target.value);
                     }}
                     value={consultation_year || ""}
-                    className="border rounded-[0.5vw] outline-none  w-full bg-[black] bg-opacity-[70%] placeholder:text-white placeholder:text-opacity-[50%] capitalize text-white md:h-[3vw] h-[8vw] px-[3%] md:text-[1vw] text-[3vw]"
+                    className="border rounded-[0.5vw] outline-none  w-full bg-[black] bg-opacity-[70%] placeholder:text-white placeholder:text-opacity-[50%] capitalize text-white md:h-[3vw] h-[8vw] px-[3%] md:text-[1vw] text-[3.5vw]"
                     placeholder="Eg : 2023, 2024.."
                   />
                 </div>
                 {/* the readmore link */}
-                <div className="flex flex-col md:gap-[0.3vw] gap-[2vw] w-full">
+                <div className="flex flex-col md:gap-[0.3vw] gap-[2vw] md:w-full w-[100%] md:mt-0 mt-[4vw]">
                   <label
                     htmlFor="year"
-                    className="capitalize md:text-[1vw] text-[3vw]"
+                    className="capitalize md:text-[1vw] text-[3.5vw]"
                   >
                     Read more link
                   </label>
@@ -224,7 +248,7 @@ const Modal_edit_consulation = ({
                       setconsulation_readmore_link(e.target.value);
                     }}
                     value={consulation_readmore_link || ""}
-                    className="border rounded-[0.5vw] outline-none bg-[black] bg-opacity-[70%] placeholder:text-white placeholder:text-opacity-[50%] normal-case text-white md:h-[3vw] h-[8vw] px-[3%] md:text-[1vw] text-[3vw]"
+                    className="border rounded-[0.5vw] outline-none bg-[black] bg-opacity-[70%] placeholder:text-white placeholder:text-opacity-[50%] normal-case text-white md:h-[3vw] h-[8vw] px-[3%] md:text-[1vw] text-[3.5vw]"
                     placeholder="Eg : https://www.eric.com/consulation "
                   />
                 </div>
@@ -232,9 +256,9 @@ const Modal_edit_consulation = ({
             </div>
           </div>
           {/* the buttons for call to action */}
-          <div className="w-full md:pt-[2vw] md:px-0 gap-[5%] px-[5%] flex justify-center  md:gap-[4vw] ">
+          <div className="w-full md:pt-[2vw] md:px-0 gap-[5%] px-[3%] flex justify-center  md:gap-[4vw] ">
             <button
-              className=" md:px-[4vw] md:w-auto w-full md:h-auto h-[10vw] md:py-[0.5vw]capitalize bg-white  md:rounded-[0.5vw] hover:bg-opacity-[60%] md:text-[1vw] text-[3vw] backdrop-blur-2xl text-center border-red-500 border"
+              className=" md:px-[4vw] md:w-auto w-full md:h-auto h-[10vw] md:py-[0.5vw]capitalize bg-white  md:rounded-[0.5vw] hover:bg-opacity-[60%] md:text-[1vw] text-[3.5vw] backdrop-blur-2xl text-center border-red-500 border"
               onClick={() => {
                 // setadd_publdcication(false);
                 setadd_consulation(false);
@@ -244,7 +268,7 @@ const Modal_edit_consulation = ({
             </button>
             <button
               type="submit"
-              className=" md:px-[4vw] md:py-[0.5vw] md:w-auto w-full md:h-auto h-[10vw] capitalize text-white  md:rounded-[0.5vw] hover:bg-opacity-[60%] md:text-[1vw] text-[3vw] backdrop-blur-2xl text-center bg-red-500 border"
+              className=" md:px-[4vw] md:py-[0.5vw] md:w-auto w-full md:h-auto h-[10vw] capitalize text-white  md:rounded-[0.5vw] hover:bg-opacity-[60%] md:text-[1vw] text-[3.5vw] backdrop-blur-2xl text-center bg-red-500 border"
             >
               upload consulation
             </button>
@@ -267,7 +291,7 @@ const Modal_edit_consulation = ({
             <h2 className=" font-bold mb-4 md:text-[1.2vw]">
               How to Add Links
             </h2>
-            <p className="text-red-500 md:text-[1vw] text-[3vw]">Note:</p>
+            <p className="text-red-500 md:text-[1vw] text-[3.5vw]">Note:</p>
             <p className="md:text-[0.9vw]">
               Please use the following HTML tag to add hyperlinks within your
               description.
