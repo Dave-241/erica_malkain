e11034ac7a0371308d65ddc13ccf8b9bef890ad0
DAVIS ISIBOR <davisisibor@gmail.com>
2024-08-03 09:44:26 +0100

    Update modal_edit_research.tsx

--- src/app/components/research/modal_edit_research.tsx
+++ src/app/components/research/modal_edit_research.tsx
@@ -158,142 +158,145 @@ const Modal_edit_research = ({
       )}
 
       <div
-        className={`w-full md:h-full min-h-[100vh] md:py-0 py-[20vw] fixed top-0  ${
+        className={`w-full h-full min-h-[100vh] md:py-0 py-[8vw] fixed top-0  ${
           !open_edit ? "left-[-100%]" : "left-0"
         }
-       bg-white bg-opacity-[100%] overflow-x-hidden overflow-y-scroll z-[1000] flex md:justify-around justify-start md:flex-row  flex-col  items-center`}
+       bg-white bg-opacity-[100%] flex  justify-center md:items-center overflow-x-hidden overflow-y-scroll z-[1000] `}
       >
-        <div className="bg-white md:px-[5%] justify-center md:rounded-[1vw]  md:py-[3vw] py-[10vw]  w-[95%] px-[3%] md:w-[30vw]  rounded-[5vw] flex md:gap-[1vw] capitalize flex-col gap-[6vw]">
-          {!edit_ID || (edit_ID && article_link) ? (
-            <>
-              {" "}
-              <p className="md:text-[2vw] text-[8vw] text-center">
+        <div className="w-full h-fit   flex md:justify-around md:gap-0 gap-[10vw] justify-start items-center md:items-start md:flex-row  flex-col  ">
+          {/* this is for the editor  */}
+          <Editor
+            key={editorKey}
+            apiKey={"o6poh8mrrg3olm60uzci8redu8zma5ystr23b8f78hku2msu"} // your api key here
+            onEditorChange={(newValue, editor) =>
+              onEditorInputChange(newValue, editor)
+            }
+            onInit={(evt, editor) => setText(editor.getContent())}
+            value={value}
+            init={{
+              height: calWidth < 768 ? "80vh" : "80vh",
+              width: calWidth < 768 ? "95%" : "60vw",
+              plugins: plugins,
+              toolbar: toolbars,
+            }}
+            initialValue={"Write your research here..."}
+          />{" "}
+          {/* FIELDS TO ADD CONTENT  */}
+          <div className="bg-white md:px-[5%] justify-center md:rounded-[1vw]  md:py-[3vw] py-[10vw]  w-[95%] px-[3%] md:w-[30vw]  rounded-[5vw] flex md:gap-[1vw] capitalize flex-col gap-[6vw]">
+            {!edit_ID || (edit_ID && article_link) ? (
+              <>
                 {" "}
-                {edit_ID ? "edit" : "Add "} Research here
-              </p>
-              {/* title and image section */}
-              <div className="md:flex-row gap-[4vw] flex-col flex md:items-end justify-center md:gap-[1vw]">
-                <div className="flex w-full  md:gap-[1vw] gap-[5%] ">
-                  <button
-                    style={{ whiteSpace: "nowrap" }}
-                    className="  md:h-[3vw]  h-[13vw] w-full md:text-[0.9vw] md:px-[1.3vw] bg-[#103210] text-white md:rounded-[0.5vw] hover:bg-white hover:text-black  rounded-[2vw] hover:border-black border-[#103210] border"
-                    onClick={() => {
-                      setopen_img(true);
-                    }}
-                  >
-                    {image_link ? "Replace" : "Choose"} research Image
-                  </button>
-                  {image_link && (
-                    <div className="md:h-[5vw]   h-[13vw] w-full md:rounded-[0.5vw] rounded-[2vw] relative overflow-hidden">
-                      <Image
-                        src={image_link}
-                        unoptimized
-                        width="0"
-                        height="0"
-                        alt="image link"
-                        className="w-full  absolute  absolute_center h-fit"
-                      />
-                    </div>
-                  )}
+                <p className="md:text-[2vw] text-[8vw] text-center">
+                  {" "}
+                  {edit_ID ? "edit" : "Add "} Research here
+                </p>
+                {/* title and image section */}
+                <div className="md:flex-row gap-[4vw] flex-col flex md:items-end justify-center md:gap-[1vw]">
+                  <div className="flex w-full  md:gap-[1vw] gap-[5%] ">
+                    <button
+                      style={{ whiteSpace: "nowrap" }}
+                      className="  md:h-[3vw]  h-[13vw] w-full md:text-[0.9vw] md:px-[1.3vw] bg-[#103210] text-white md:rounded-[0.5vw] hover:bg-white hover:text-black  rounded-[2vw] hover:border-black border-[#103210] border"
+                      onClick={() => {
+                        setopen_img(true);
+                      }}
+                    >
+                      {image_link ? "Replace" : "Choose"} research Image
+                    </button>
+                    {image_link && (
+                      <div className="md:h-[5vw]   h-[13vw] w-full md:rounded-[0.5vw] rounded-[2vw] relative overflow-hidden">
+                        <Image
+                          src={image_link}
+                          unoptimized
+                          width="0"
+                          height="0"
+                          alt="image link"
+                          className="w-full  absolute  absolute_center h-fit"
+                        />
+                      </div>
+                    )}
+                  </div>
                 </div>
-              </div>
-              {/* the links for viewing */}
-              <div className="w-full flex md:gap-[2vw] gap-[5%]  justify-between">
-                {/* the download link */}
-                <div className="flex flex-col md:gap-[0.3vw] gap-[2vw] w-full">
+                {/* the links for viewing */}
+                <div className="w-full flex md:gap-[2vw] gap-[5%]  justify-between">
+                  {/* the download link */}
+                  <div className="flex flex-col md:gap-[0.3vw] gap-[2vw] w-full">
+                    <label
+                      className="capitalize md:text-[1vw] text-[3.5vw]"
+                      htmlFor="pdf_link"
+                    >
+                      Research title
+                    </label>
+                    <input
+                      type="text"
+                      id="pdf_link"
+                      onChange={(e) => {
+                        setarticle_link(e.target.value);
+                      }}
+                      value={article_link || ""}
+                      className="border md:rounded-[0.5vw] outline-none bg-[black] bg-opacity-[70%] placeholder:text-white capitalize text-white md:h-[3vw] w-full h-[10vw] rounded-[1.5vw] px-[3%] md:text-[1vw] text-[3.5vw]"
+                      placeholder="input your Research title here .."
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-col md:gap-[0.3vw] gap-[2vw]">
                   <label
                     className="capitalize md:text-[1vw] text-[3.5vw]"
-                    htmlFor="pdf_link"
+                    htmlFor="description"
                   >
-                    Research title
+                    Research caption
                   </label>
-                  <input
-                    type="text"
-                    id="pdf_link"
+                  <textarea
+                    //   type="text"
+                    id="description"
+                    rows={4}
+                    //   rows={50}
+                    value={caption || ""}
                     onChange={(e) => {
-                      setarticle_link(e.target.value);
+                      setcaption(e.target.value);
                     }}
-                    value={article_link || ""}
-                    className="border md:rounded-[0.5vw] outline-none bg-[black] bg-opacity-[70%] placeholder:text-white capitalize text-white md:h-[3vw] w-full h-[10vw] rounded-[1.5vw] px-[3%] md:text-[1vw] text-[3.5vw]"
-                    placeholder="input your Research title here .."
+                    className="border  md:rounded-[1vw] rounded-[1.5vw]  outline-none bg-[black] bg-opacity-[70%] placeholder:text-white capitalize text-white resize-none p-[2%] md:text-[1vw] text-[3.5vw]"
+                    placeholder="input Research caption here .."
                   />
                 </div>
-              </div>
-              <div className="flex flex-col md:gap-[0.3vw] gap-[2vw]">
-                <label
-                  className="capitalize md:text-[1vw] text-[3.5vw]"
-                  htmlFor="description"
-                >
-                  Research caption
-                </label>
-                <textarea
-                  //   type="text"
-                  id="description"
-                  rows={4}
-                  //   rows={50}
-                  value={caption || ""}
-                  onChange={(e) => {
-                    setcaption(e.target.value);
-                  }}
-                  className="border  md:rounded-[1vw] rounded-[1.5vw]  outline-none bg-[black] bg-opacity-[70%] placeholder:text-white capitalize text-white resize-none p-[2%] md:text-[1vw] text-[3.5vw]"
-                  placeholder="input Research caption here .."
-                />
-              </div>
-              {error && (
-                <p className="text-red-500 md:text-[1vw] text-[3.5vw]">
-                  {error}
-                </p>
-              )}
-              <div className="w-full md:pt-[2vw] flex justify-center gap-[5%]  md:gap-[2vw] ">
+                {error && (
+                  <p className="text-red-500 md:text-[1vw] text-[3.5vw]">
+                    {error}
+                  </p>
+                )}
+                <div className="w-full md:pt-[2vw] flex justify-center gap-[5%]  md:gap-[2vw] ">
+                  <button
+                    className=" w-full py-[2.6vw]  md:py-[0.5vw] capitalize bg-white  rounded-[2.5vw] md:rounded-[0.5vw] hover:bg-opacity-[60%] md:text-[1vw] text-[3.5vw] backdrop-blur-2xl text-center border-red-500 border"
+                    onClick={() => {
+                      setopen_edit(false);
+                    }}
+                  >
+                    close editor
+                  </button>
+                  <button
+                    onClick={submit_form}
+                    disabled={loading}
+                    type="submit"
+                    className=" w-full py-[2.6vw]  md:py-[0.5vw] capitalize text-white  rounded-[2.5vw] md:rounded-[0.5vw] hover:bg-opacity-[60%] md:text-[1vw] backdrop-blur-2xl text-center bg-red-500 border"
+                  >
+                    {loading ? "Uploading..." : "Confirm Upload"}{" "}
+                  </button>
+                </div>
+              </>
+            ) : (
+              <>
+                <p>loading ...</p>
                 <button
-                  className=" w-full py-[2.6vw]  md:py-[0.5vw] capitalize bg-white  rounded-[2.5vw] md:rounded-[0.5vw] hover:bg-opacity-[60%] md:text-[1vw] text-[3.5vw] backdrop-blur-2xl text-center border-red-500 border"
+                  className=" md:px-[4vw] md:w-auto w-full py-[2.6vw]  md:py-[0.5vw] capitalize bg-white  rounded-[2.5vw] md:rounded-[0.5vw] hover:bg-opacity-[60%] md:text-[1vw] text-[3.5vw] backdrop-blur-2xl text-center border-red-500 border"
                   onClick={() => {
                     setopen_edit(false);
                   }}
                 >
-                  close editor
+                  Cancel
                 </button>
-                <button
-                  onClick={submit_form}
-                  disabled={loading}
-                  type="submit"
-                  className=" w-full py-[2.6vw]  md:py-[0.5vw] capitalize text-white  rounded-[2.5vw] md:rounded-[0.5vw] hover:bg-opacity-[60%] md:text-[1vw] backdrop-blur-2xl text-center bg-red-500 border"
-                >
-                  {loading ? "Uploading..." : "Confirm Upload"}{" "}
-                </button>
-              </div>
-            </>
-          ) : (
-            <>
-              <p>loading ...</p>
-              <button
-                className=" md:px-[4vw] md:w-auto w-full py-[2.6vw]  md:py-[0.5vw] capitalize bg-white  rounded-[2.5vw] md:rounded-[0.5vw] hover:bg-opacity-[60%] md:text-[1vw] text-[3.5vw] backdrop-blur-2xl text-center border-red-500 border"
-                onClick={() => {
-                  setopen_edit(false);
-                }}
-              >
-                Cancel
-              </button>
-            </>
-          )}{" "}
+              </>
+            )}{" "}
+          </div>
         </div>
-        {/* this is for the editor  */}
-        <Editor
-          key={editorKey}
-          apiKey={"o6poh8mrrg3olm60uzci8redu8zma5ystr23b8f78hku2msu"} // your api key here
-          onEditorChange={(newValue, editor) =>
-            onEditorInputChange(newValue, editor)
-          }
-          onInit={(evt, editor) => setText(editor.getContent())}
-          value={value}
-          init={{
-            height: calWidth < 768 ? "50vh" : "80vh",
-            width: calWidth < 768 ? "95%" : "60vw",
-            plugins: plugins,
-            toolbar: toolbars,
-          }}
-          initialValue={"Write your research here..."}
-        />{" "}
       </div>
     </>
   );
