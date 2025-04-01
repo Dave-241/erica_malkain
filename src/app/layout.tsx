af4c8286dbc4d06642cdf8e2f7f47a0fd2f28740
Davis Isibor <davisisibor@gmail.com>
2025-04-01 18:40:08 +0100

    update bing tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Erica J. Boothby - Psychologist, Researcher, and Educator",
@@ -14,6 +15,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      {/* bing tracking  */}
+      <Script
+        id="Bing clarity"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", '${process.env.NEXT_PUBLIC_BING_CLARITY_ID}');`,
+        }}
+      />
       <body className={``} style={{ backgroundColor: "#DFE4DF" }}>
         {children}
       </body>
